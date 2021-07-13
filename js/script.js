a00ce0deae5ab88ba34ac6c7df4130df9ece9c7c
education <78236013+sonimakumari@users.noreply.github.com>
2021-07-13 12:42:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener(
   "DOMContentLoaded",
   function(event){
-    alert('The page is loaded and the script ran!')
     var message = document.getElementById('message')
     message.innerText = 'The script ran!'
   }
